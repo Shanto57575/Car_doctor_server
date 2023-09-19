14676698d8a39a07d442751d739db826f2b084d5
Shanto57575 <shanto57575@gmail.com>
2023-09-20 01:38:11 +0600

    sort and search functionality updated

--- index.js
+++ index.js
@@ -61,8 +61,7 @@ async function run() {
         app.get('/services', async (req, res) => {
             const sort = req.query.sort
             const search = req.query.search
-            console.log(search);
-            const query = { title: { $regex: search, options: 'i' } };
+            const query = { title: { $regex: search, $options: 'i' } };
             const options = {
                 sort: {
                     "price": sort == 1 ? 1 : -1
